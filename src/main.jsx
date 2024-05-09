c112ad88410ef6048fcc64894f94d9c513787779
TanveerAhmed4545 <tanveer8507@gmail.com>
2024-05-09 22:28:21 +0600

    add auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from './routes/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
+import AuthProvider from './providers/AuthProvider.jsx'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <AuthProvider>
+      <HelmetProvider>
+      <RouterProvider router={router} />
+      <ToastContainer/>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
